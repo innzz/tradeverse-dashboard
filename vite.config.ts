b26e8d1c4e9d51f1d4aeef98e81ec68e7d5ed5e0
inzamam <inzamam.pawaskar@originbluy.com>
2024-01-13 10:15:27 +0530

    feat: Integrated Docker configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  },
+  preview: {
+    host: true,
+    port: 5173
+  },
   plugins: [react()],
 })
